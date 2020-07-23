5dfc5f725d6a69197481061d6e6676fdb221adfd
Cedric Canovas <dev@canovas.me>
2020-07-23 09:03:32 +0200

    Adapt markdown for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().replace("```py", "```")
 
 setuptools.setup(
     name="dataenforce",
